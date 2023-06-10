80294b1f93ec014109cae002cf0a0ea1466b324a
jesslyn-ctrl <jsslnjess08@gmail.com>
2023-06-10 16:29:32 +0700

    add daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <button className="btn">Button</button>
+    </>
   )
 }
 
