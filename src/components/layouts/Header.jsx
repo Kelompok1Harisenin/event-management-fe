256e08b66c6a0f85e48600f5b8ef50963332a6a6
ILHAM TATAYO LIE <70442926+BOcort@users.noreply.github.com>
2023-06-11 21:45:25 +0900

    struktur header

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -8,8 +8,14 @@ function Header(props) {
         </div>
         <div className="header-list">
           <ul>
-            <li>list</li>
+            <li>Organize</li>
+            <li>Help</li>
+            <li><button>Create Event</button></li>
           </ul>
+        </div>
+        <div className="header-acc">
+            <li><button>Log In</button></li>
+            <li><button>Sign Up</button></li>
         </div>
       </header>
     </>
