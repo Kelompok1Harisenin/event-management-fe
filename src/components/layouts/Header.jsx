bf52ff1b0b240a71916b49e2024210f1f5aa928e
ILHAM TATAYO LIE <70442926+BOcort@users.noreply.github.com>
2023-07-01 15:17:57 +0900

    header-html

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,25 +1,50 @@
 import { useState } from "react";
-
-
 function Header(props) {
   return (
     <>
-      <header className="">
-        <div className="w-11/12 mx-auto grid grid-cols-3 gap-4">
-          <div className=" text-xl" id="header-logo">
-            EVENT MANAGEMENT
+      <header>
+        <div id="container" className="flex">
+          {/* Iconya */}
+          <div id="header-list" className="float-left">
+            Event Management
           </div>
-          <div className="" id="search-bar">
-            <input type="text" placeholder="FIND EVENT?"/>
+          {/* Search Bar */}
+          <div id="Search-bar" className="align-middle">
+            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
           </div>
-          <div className="" id="sign">
-            <button></button>
+          {/* Header list */}
+          <div id="header-list" className="flex ">
+            <ul id="list-event" className="flex float-right">
+              {/* Organize */}
+              <li id="organize">
+                Organize
+                <ul id="organize-list">
+                  <li><a href="">Create Event</a></li>
+                  <li><a href="">Pricing</a></li>
+                  <li><a href="">Resource</a></li>
+                  <li><a href="">Contact Sales</a></li>
+                </ul>
+              </li>
+              {/* Help */}
+              <li id="Help">
+                Help
+                <ul id="Help-list">
+                  <li><a href="">Find Your Ticket</a></li>
+                  <li><a href="">Contact an Event Organizer</a></li>
+                  <li><a href="">Visit The Help Center</a></li>
+                </ul>
+              </li>
+            </ul>
+            <ul id="list-acc" className="flex">
+              <li><a href="">Create Event</a></li>
+              <li><a href="">Log In</a></li>
+              <li><a href="">Sign Up</a></li>
+            </ul>
           </div>
         </div>
       </header>
     </>
   )
 }
-
 
 export default Header;
