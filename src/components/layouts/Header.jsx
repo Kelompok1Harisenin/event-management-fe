3394b41d0582b6c243e84f53f066b22769dd8295
ILHAM TATAYO LIE <70442926+BOcort@users.noreply.github.com>
2023-07-02 17:51:26 +0900

    header-html

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -4,14 +4,18 @@ function Header(props) {
   return (
 
     <header>
-      <div id="container" className="flex">
+      <div id="container" className="grid grid-cols-3">
         {/* Iconya */}
         <div id="header-list" className="float-left">
           Event Management
         </div>
         {/* Search Bar */}
         <div id="Search-bar" className="align-middle">
-          <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+          <div class="relative">
+            <i class="absolute fa fa-search text-gray-400 top-5 left-4"></i>
+            <input type="text" class="bg-white h-14 w-full px-12 rounded-lg focus:outline-none hover:cursor-pointer" name=""/>
+              <span class="absolute top-4 right-5 border-l pl-4"><i class="fa fa-microphone text-gray-500 hover:text-green-500 hover:cursor-pointer"></i></span>
+          </div>
         </div>
         {/* Header list */}
         <div id="header-list" className="flex ">
@@ -19,7 +23,7 @@ function Header(props) {
             {/* Organize */}
             <li id="organize">
               Organize
-              <ul id="organize-list">
+              <ul id="organize-list" className="hidden">
                 <li><a href="">Create Event</a></li>
                 <li><a href="">Pricing</a></li>
                 <li><a href="">Resource</a></li>
@@ -29,7 +33,7 @@ function Header(props) {
             {/* Help */}
             <li id="Help">
               Help
-              <ul id="Help-list">
+              <ul id="Help-list" className="hidden">
                 <li><a href="">Find Your Ticket</a></li>
                 <li><a href="">Contact an Event Organizer</a></li>
                 <li><a href="">Visit The Help Center</a></li>
