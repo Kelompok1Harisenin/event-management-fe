a879299ed57e3b6f08c9469e9eb968a0bc788c4a
ILHAM TATAYO LIE <70442926+BOcort@users.noreply.github.com>
2023-07-08 15:09:47 +0900

    header jsx 2.0

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -6,9 +6,13 @@ import { FaBars, FaBeer, FaSearch } from 'react-icons/fa';
 function Header(props) {
   const [isHovered, setIsHovered] = useState({
     organize: false,
-    help: false
+    help: false,
+    bars : false
   });
+  const [isPressed, setIsPressed] = useState(false);
 
+
+  // Hover & Click
   const handleMouseEnter = (id) => {
     setIsHovered((prevState) => ({
       ...prevState,
@@ -23,44 +27,55 @@ function Header(props) {
     }));
   };
 
+  const handleClick = () => {
+    setIsPressed((prevState) => (!prevState))
+  }
+
+  const maxMd = (array) => {
+    return array.map((value) => {
+      return `max-md:${value}`
+    })
+  };
+
   return (
-    <header className="bg-white text-black h-20">
-      <div id="container" className="grid grid-cols-7 gap-2 w-4/5 mx-auto h-full">
+    <header className="bg-white text-black h-20 w-full">
+      <div id="container" className={`grid grid-cols-7 gap-2 w-4/5 mx-auto h-full max-md:grid-cols-6 max-md:w-[95%] `}>
         {/* Iconya */}
-        <div id="header-icon" className="float-left text-center h-5/6">
+        <div id="header-icon" className="float-left text-center h-5/6 max-md:cols-span-1">
           <div id="image-logo" className="">
-            <img src={Logo} alt="logo" className="h-20" />
+            <NavLink> <img src={Logo} alt="logo" className="h-20" /></NavLink>
           </div>
         </div>
         {/* Search Bar */}
-        <div id="Search-bar" className="text-center col-span-3 py-6">
-          <label htmlFor="">
-            <input type="text" className="w-5/6 bg-gray-200 rounded-lg h-10 font-medium m-auto" /><button className="h-10 rounded-lg ml-3 px-5 bg-gray-300 text-center"><FaSearch /></button>
-          </label>
+        <div id="Search-bar" className="text-center col-span-3 py-6 max-md:col-span-4">
+          <form className="flex">
+            <input type="text" className="w-5/6 bg-gray-200 rounded-lg h-10 font-medium m-auto " /><button type='submit' className="h-10 rounded-full ml-3 px-5 hover:bg-gray-300 text-center "><FaSearch /></button>
+          </form>
         </div>
         {/* Header list */}
-        <div id="header-list" className="col-span-3 text-center">
-          <div id="header-bars" className="hidden">
-            <button><FaBars /></button>
-          </div>
-          <ul id="list-event" className="grid grid-cols-5">
+        <div id="header-list" className="col-span-3 text-center max-md:col-span-1 ">
+          {/* <button className="hidden max-md:block p-8 ml-3  hover:bg-gray-300 text-center " ><FaBars /></button> */}
+          <ul className="hidden max-md:block grid grid-cols-1">
+            <li className="col-span-1 text-center py-8 px-10 hover:bg-gray-300" onClick={() => handleClick('bars')}><FaBars/></li>
+          </ul>
+          <ul id="list-event" className={`grid grid-cols-5 max-md:${isPressed ? 'block' : 'hidden'} max-md:w-60 max-md:grid-rows-1 max-md:bg-white`}>
             {/* Organize */}
-            <li id="organize" className="col-span-1 text-center py-7 hover:bg-gray-300" onMouseEnter={() => handleMouseEnter('organize')} onMouseLeave={() => handleMouseLeave('organize')} >
+            <li id="organize" className="col-span-1 text-center py-7 hover:bg-gray-300 " onMouseEnter={() => handleMouseEnter('organize')} onMouseLeave={() => handleMouseLeave('organize')} >
               Organize
-              <ul id="organize-list" className={`${isHovered.organize ? 'block' : 'hidden'} h-full grid grid-rows-4 bg-white mt-7 w-40 rounded-lg`}>
-                <li className="h-10 row-span-1 hover:bg-gray-300"><NavLink>Create Event</NavLink></li>
-                <li className="h-10 row-span-1 hover:bg-gray-300"><NavLink>Pricing</NavLink></li>
-                <li className="h-10 row-span-1 hover:bg-gray-300"><NavLink>Resource</NavLink></li>
-                <li className="h-10 row-span-1 hover:bg-gray-300"><NavLink>Contact Sales</NavLink></li>
+              <ul id="organize-list" className={`${isHovered.organize ? 'block' : 'hidden'} h-full grid grid-rows-4 bg-white mt-7 w-40 rounded-lg `}>
+                <li className="h-full row-span-1 hover:bg-gray-300"><NavLink>Create Event</NavLink></li>
+                <li className="h-full row-span-1 hover:bg-gray-300"><NavLink>Pricing</NavLink></li>
+                <li className="h-full row-span-1 hover:bg-gray-300"><NavLink>Resource</NavLink></li>
+                <li className="h-full row-span-1 hover:bg-gray-300"><NavLink>Contact Sales</NavLink></li>
               </ul>
             </li>
             {/* Help */}
             <li id="Help" className="col-span-1 text-center py-7 hover:bg-gray-300" onMouseEnter={() => handleMouseEnter('help')} onMouseLeave={() => handleMouseLeave('help')} >
               Help
               <ul id="Help-list" className={`${isHovered.help ? 'block' : 'hidden'} h-full grid grid-rows-3 bg-white mt-7 w-40 rounded-lg`}>
-                <li className="h-10 row-span-1 hover:bg-gray-300"><NavLink >Find Your Ticket</NavLink></li>
-                <li className="h-14 row-span-1 hover:bg-gray-300"><NavLink >Contact an Event Organizer</NavLink></li>
-                <li className="h-14 row-span-1 hover:bg-gray-300"><NavLink >Visit The Help Center</NavLink></li>
+                <li className="h-full row-span-1 hover:bg-gray-300"><NavLink >Find Your Ticket</NavLink></li>
+                <li className="h-full row-span-1 hover:bg-gray-300"><NavLink >Contact an Event Organizer</NavLink></li>
+                <li className="h-full row-span-1 hover:bg-gray-300"><NavLink >Visit The Help Center</NavLink></li>
               </ul>
             </li>
             {/* Account & Create Event*/}
