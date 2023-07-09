227dff85be09df84c8450f011bc8b4a1998dfdee
achmady <achmadysurya@gmail.com>
2023-07-09 17:36:35 +0700

    fix feature/footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,2 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/layouts/Header'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    <Header />
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button className="btn">Button</button>
-    </>
-  )
-}
-
-export default App
+import React from "react";
+import Footer from "./components/layouts/Footer";
