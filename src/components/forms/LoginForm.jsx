927e21f3f2ba976f9052bc9c91a270af7397787e
jesslyn-ctrl <jsslnjess08@gmail.com>
2023-07-23 18:45:54 +0700

    add toggle password visibility

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -5,7 +5,7 @@ import { loginUser } from "./../../redux/slices/authSlice";
 import { ErrorPopup } from "./../../components";
 import Cookies from "js-cookie";
 import { Link } from "react-router-dom";
-import { authHelpers } from "./../../utils";
+import { RiEyeFill, RiEyeOffFill } from "react-icons/ri";
 
 const LoginForm = () => {
   const dispatch = useDispatch();
@@ -14,6 +14,7 @@ const LoginForm = () => {
     email: "",
     password: "",
   });
+  const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState(null);
 
   const handleChange = (e) => {
@@ -25,9 +26,7 @@ const LoginForm = () => {
     e.preventDefault();
 
     try {
-      const hashedPassword = await authHelpers.hashPassword(formData.password);
-      const loginData = { ...formData, password: hashedPassword };
-      const result = await dispatch(loginUser(loginData));
+      const result = await dispatch(loginUser(formData));
       if (result.payload) {
         const tokens = result.payload.tokens;
         Cookies.set("user", JSON.stringify(result.payload.user));
@@ -44,6 +43,10 @@ const LoginForm = () => {
 
   const closeErrorPopup = () => {
     setError(null);
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
   };
 
   return (
@@ -66,33 +69,45 @@ const LoginForm = () => {
             onChange={handleChange}
           />
         </div>
-        <div className="form-control mb-6">
+        <div className="form-control mb-6 relative">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
           <input
-            type="password"
+            type={showPassword ? "text" : "password"}
             name="password"
             placeholder="Password must be at least 8 characters"
             className="input input-bordered input-warning w-full hover:border-orange-300 hover:border-2 transition duration-200 ease-in-out"
             value={formData.password}
             onChange={handleChange}
+            autoComplete="off"
           />
-        </div>
 
-        <div className="flex justify-right label text-sm  text-blue-500 hover:text-blue-600 hover:underline">
-          <span></span>
-          <Link to="/forgot">Forgot Password?</Link>
-        </div>
-
-        <div className="flex justify-center">
-          <button
-            type="submit"
-            className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 w-full rounded-md transition duration-200 ease-in-out"
+          {/* Eye icon to toggle password visibility */}
+          <div
+            className="absolute mt-[52px] text-slate-500 right-4 transform cursor-pointer"
+            onClick={togglePasswordVisibility}
           >
-            Login
-          </button>
+            {showPassword ? <RiEyeOffFill /> : <RiEyeFill />}
+          </div>
         </div>
+
+        <div className="mt-4 flex justify-between">
+          <form className="flex gap-2">
+            <input type="checkbox" />
+            <label htmlFor="checkbox">Remember me</label>
+          </form>
+          <Link to="/forgot" className="text-blue-500 hover:text-blue-600">
+            Forgot password
+          </Link>
+        </div>
+
+        <button
+          type="submit"
+          className="mt-5 bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 w-full rounded-md transition duration-200 ease-in-out"
+        >
+          Login
+        </button>
 
         <div className="flex justify-center label text-sm text-black">
           Don&apos;t have an account?
