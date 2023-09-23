04ec0a51f61f0a3319ac55e5a44dba79be186d09
ILHAM TATAYO LIE <70442926+BOcort@users.noreply.github.com>
2023-09-23 12:22:12 +0900

    mobile version

--- src/components/popup/ReserveTicketPopup.jsx
+++ src/components/popup/ReserveTicketPopup.jsx
@@ -1,5 +1,4 @@
-// import React from 'react'
-
+// import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { closePopup } from './action';
 
@@ -10,39 +9,47 @@ export default function ReserveTicketPopup() {
   const handleCancel = () => {
     dispatch(closePopup());
   };
+
   return (
     isPopupVisible && (
-    <div id="behind" className={`w-full h-full fixed top-0 left-0 bg-black/60`}>
-      <div className=" w-5/6 h-4/5 mx-auto mt-24 grid grid-cols-5">
-        <div className=" col-span-3 bg-white rounded-lg relative z-10">
-          <div className="w-11/12 h-full mx-auto grid grid-rows-8">
-            <div className=" text-center row-span-1"><h1 className=" text-3xl my-2">Title</h1></div>
-            <div className=" my-5 row-span-6 snap-y">
-              <p className=" text-lg">about : Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatem doloremque accusantium nisi! Commodi totam ratione, neque perspiciatis placeat amet facere molestiae vitae obcaecati qui itaque non culpa dolorem alias? Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ducimus tempore quo magni dolorum perspiciatis beatae voluptate voluptates. Itaque numquam dicta rem eveniet! Nihil dolorem error inventore reprehenderit quam sunt Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium error iure corporis voluptate obcaecati necessitatibus blanditiis quas voluptatibus eos pariatur assumenda doloremque, sapiente fuga? Commodi id deleniti beatae est voluptatem.</p>
-              <div id="price" className="flex">
-                <div className="mr-auto">
-                  (1x) Ticket
+      <div id="behind" className="w-full h-full fixed top-0 left-0 bg-black/60 overflow-auto">
+        <div className="w-5/6 h-4/5 mx-auto mt-24 md:grid md:grid-cols-5">
+          <div className="md:col-span-3 bg-white rounded-lg relative z-10 overflow-y-auto">
+            <div className="w-11/12 h-full mx-auto grid grid-rows-8 snap-y">
+              <div className="text-center row-span-1">
+                <h1 className="text-3xl my-2">Title</h1>
+              </div>
+              <div id="detail" className="row-span-6">
+                <div className="md:hidden">
+                  <img src="/images/gambar7.png" alt="" className="w-full items-center max-md:mx-auto" />
                 </div>
-                <div className="mr-10">
-                  free
+                <p className="text-lg">
+                  about : Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatem doloremque accusantium
+                  nisi! Commodi totam ratione, neque perspiciatis placeat amet facere molestiae vitae obcaecati qui itaque
+                  non culpa dolorem alias? Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ducimus
+                  tempore quo magni dolorum perspiciatis beatae voluptate voluptates. Itaque numquam dicta rem eveniet!
+                  Nihil dolorem error inventore reprehenderit quam sunt Lorem ipsum dolor, sit amet consectetur adipisicing
+                  elit. Praesentium error iure corporis voluptate obcaecati necessitatibus blanditiis quas voluptatibus eos
+                  pariatur assumenda doloremque, sapiente fuga? Commodi id deleniti beatae est voluptatem.
+                </p>
+                <div id="price" className="flex my-5">
+                  <div className="mr-auto">(1x) Ticket</div>
+                  <div className="mr-10">free</div>
                 </div>
               </div>
             </div>
-            <div className="flex  row-span-1">
-              <div className="flex w-1/2 m-auto">
-                <button className="btn btn-error ml-auto mr-3" onClick={handleCancel}>Cancel</button>
-                <button className="btn btn-success mr-auto ml-">Buying</button>
-              </div>
+            <div className="flex row-span-1 justify-center relative md:bottom-20 max-md:bottom-10">
+              <button className="btn btn-error" onClick={handleCancel}>
+                Cancel
+              </button>
+              <button className="btn btn-success ml-3">Buying</button>
             </div>
           </div>
-        </div>
-        <div className=" col-span-2 bg-transparent flex">
-          <img src="/images/gambar7.png" alt="" className="w-full my-auto items-center" />
+          <div className="md:col-span-2 bg-transparent flex max-md:fixed max-md:top-0 z-50 max-md:hidden">
+            <img src="/images/gambar7.png" alt="" className="w-full my-auto items-center max-md:mx-auto" />
+          </div>
         </div>
       </div>
-    </div>
     )
-  )
-
-
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,6 @@ export default {
       gridTemplateRows: {
         // Simple 8 row grid
         '8': 'repeat(8, minmax(0, 1fr))',
-
-        // Complex site-specific row configuration
-        'layout': '200px minmax(900px, 1fr) 100px',
       }
     },
   },
